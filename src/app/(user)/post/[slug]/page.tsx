8667672323a69168f2229b85ed72bace0f6868e4
M-Shourav <muhammadshourav625@gmail.com>
2025-04-22 16:32:56 +0600

    singlepage complete

--- src/app/(user)/post/[slug]/page.tsx
+++ src/app/(user)/post/[slug]/page.tsx
@@ -2,10 +2,13 @@ import Container from "@/component/container";
 import { urlFor } from "@/sanity/lib/image";
 import { getSinglePost } from "@/sanity/querise";
 import dayjs from "dayjs";
+import { PortableText } from "next-sanity";
 import Image from "next/image";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 import React from "react";
+import { FaChevronLeft } from "react-icons/fa6";
+import { LuChevronsLeft } from "react-icons/lu";
 
 export default async function SinglePostPage({
   params,
@@ -65,6 +68,131 @@ export default async function SinglePostPage({
                     className="mb-10 aspect-[3/2] shadow-xl rounded-2xl object-cover"
                   />
                 )}
+                {post?.body && (
+                  <PortableText
+                    value={post?.body}
+                    components={{
+                      block: {
+                        normal: ({ children }) => (
+                          <p className="my-2 text-base/6 first:mt-0 last:mb-0">
+                            {children}
+                          </p>
+                        ),
+                        h2: ({ children }) => (
+                          <h2
+                            className="my-5 text-2xl/8 font-medium text-gray-950 
+                           tracking-tight first:mt-0 last:mb-0"
+                          >
+                            {children}
+                          </h2>
+                        ),
+                        h3: ({ children }) => (
+                          <h3
+                            className="my-5 text-xl/8 font-medium tracking-tight text-gray-950
+                          first:mt-0 last:mb-0"
+                          >
+                            {children}
+                          </h3>
+                        ),
+                        blockquote: ({ children }) => (
+                          <blockquote
+                            className="my-2.5 border-l-2 border-l-gray-300 pl-6 text-base/8 
+                          text-gray-950 first:mt-0 last:mb-0"
+                          >
+                            {children}
+                          </blockquote>
+                        ),
+                      },
+                      types: {
+                        image: ({ value }) => (
+                          <Image
+                            src={urlFor(value).url()}
+                            alt={value.alt || ""}
+                            className="w-full rounded-2xl"
+                            width={1400}
+                            height={1000}
+                          />
+                        ),
+                        separator: ({ value }) => {
+                          switch (value.style) {
+                            case "line":
+                              return (
+                                <hr className="my-8 border-t border-gray-200" />
+                              );
+                            case "space":
+                              return <div className="my-8" />;
+                            default:
+                              return null;
+                          }
+                        },
+                      },
+                      list: {
+                        bullet: ({ children }) => (
+                          <ul className="list-disc pl-4 text-base/8 marker:text-gray-400">
+                            {children}
+                          </ul>
+                        ),
+                        number: ({ children }) => (
+                          <ol className="list-decimal pl-4 text-base/8 marker:text-gray-400">
+                            {children}
+                          </ol>
+                        ),
+                      },
+                      listItem: {
+                        bullet: ({ children }) => {
+                          return (
+                            <li className="my-2 pl-2 has-[br]:mb-8">
+                              {children}
+                            </li>
+                          );
+                        },
+                        number: ({ children }) => {
+                          return (
+                            <li className="my-2 pl-2 has-[br]:mb-8">
+                              {children}
+                            </li>
+                          );
+                        },
+                      },
+                      marks: {
+                        strong: ({ children }) => (
+                          <strong className="font-semibold text-gray-950">
+                            {children}
+                          </strong>
+                        ),
+                        code: ({ children }) => (
+                          <>
+                            <span aria-hidden>`</span>
+                            <code className="text-[15px]/8 font-semibold text-gray-950">
+                              {children}
+                            </code>
+                            <span aria-hidden>`</span>
+                          </>
+                        ),
+                        link: ({ value, children }) => {
+                          return (
+                            <Link
+                              href={value.href}
+                              className="font-medium text-gray-950 underline decoration-gray-400 underline-offset-4 data-[hover]:decoration-gray-600"
+                            >
+                              {children}
+                            </Link>
+                          );
+                        },
+                      },
+                    }}
+                  />
+                )}
+                <div className="mt-10">
+                  <Link
+                    className="w-fit flex items-center gap-x-1 border border-gray-500 rounded-full
+                     shadow-2xl px-3 py-1 font-medium hover:bg-black hover:text-white duration-300 ease-in-out"
+                    href={"/"}
+                  >
+                    <FaChevronLeft className="size-4" />
+                    Back to Blog
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
